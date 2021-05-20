a0a3e3aad8c10adcac04c333376222f678e63ccb
Swetha <swetharadh10@gmail.com>
2021-05-20 16:58:26 +0530

    first commit

---
+++ YummyTummyPizza/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YummyTummyPizza.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return View();
+        }
+        public ActionResult VegPizza()
+        {
+            return View();
+        }
+        public ActionResult NonVegPizza()
+        {
+            return View();
+        }
+        public ActionResult Sides()
+        {
+            return View();
+        }
+        public ActionResult MyOrder()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ YummyTummyPizza/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YummyTummyPizza.MvcApplication" Language="C#" %>
